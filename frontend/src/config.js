8b97d3ff74269212e90d0e5cbad106b925dae934
Ubuntu <ubuntu@ip-172-31-4-114.us-west-1.compute.internal>
2025-03-04 04:07:01 +0000

    Initial commit for TravelMemory deployment

---
+++ frontend/src/config.js
@@ -0,0 +1,3 @@
+export const API_BASE_URL = "http://<YOUR_EC2_PUBLIC_IP>:3000";export const API_BASE_URL = "http://<YOUR_EC2_PUBLIC_IP>:3000";
+// For development, you might use:
+export const API_BASE_URL = "http://54.67.50.69:3000";
